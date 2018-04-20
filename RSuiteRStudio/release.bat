d0577ecb11b4abf4b0e97e37500feda51329a025
ws171913 <ws171913@wlogsolutions.com>
2018-04-20 17:48:41 +0200

    Script to release RSuiteRStudio plugin

---
+++ RSuiteRStudio/release.bat
@@ -0,0 +1,21 @@
+@echo off
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`git tag`) DO (
+    SET gitver=%%F
+)
+
+IF "%gitver%" == "" (
+    echo ERROR: No GIT tag detected.
+    goto error
+)
+
+set pkg_index=%~dp0\PKG_INDEX
+del "%pkg_index%" 2> nul
+
+echo Building/uploading RSuiteRStudio tag %gitver% onto S3 repository ...
+call rsuite proj depsinst -v
+if ERRORLEVEL 1 goto error
+call rsuite repo addproj -s http://wlog-rsuite.s3.amazonaws.com -b F -v
+if ERRORLEVEL 1 goto error
+echo Building/uploading RSuiteRStudio tag %gitver% onto S3 repository ... done
+
