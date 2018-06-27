6fadc394b730fa52c478008a4605daf658e58881
Ryszard Szymanski <ryszard.szymanski@outlook.com>
2018-06-27 11:26:33 +0200

    Add test cases for prj_clean_deps_app

---
+++ RSuiteRStudio/tests/apps/rstudio_05_prj_clean_deps/tests/non_existing_folder.R
@@ -0,0 +1,6 @@
+app <- ShinyDriver$new("../")
+app$snapshotInit("non_existing_folder")
+
+app$setInputs(project_folder = "$ghost_dir")
+app$setInputs(start_btn = "click")
+app$snapshot(list(output = "project_folder_err"))
