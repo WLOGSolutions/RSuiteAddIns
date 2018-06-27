d5e70d8370d57bd84e9a4c775f5b2618525703e9
Ryszard Szymanski <ryszard.szymanski@outlook.com>
2018-06-27 14:05:52 +0200

    Rename prj_zip test script to follow overall convention

---
+++ RSuiteRStudio/tests/test_proj_zip_app.R
@@ -0,0 +1,48 @@
+#----------------------------------------------------------------------------
+# RSuite
+# Copyright (c) 2017, WLOG Solutions
+#----------------------------------------------------------------------------
+
+library(shinytest)
+library(testthat)
+
+source("R/test_utils.R")
+source("R/project_managment.R")
+app_dir = "apps/rstudio_06_prj_zip/"
+
+context("Checking if addin for RSuite pkgzip creation works properly")
+
+test_that_shiny_app(app_dir, "Correct pkgzip creation", {
+  # Prepare project
+  prj <- init_test_project(repo_adapters = c("Dir"))
+  params <- prj$load_params()
+
+  # Prepare repo
+  deploy_package_to_lrepo(pkg_file = "logging_0.7-103.tar.gz", prj = prj, type = "source")
+
+  # Prepare package
+  create_test_package("TestPackage", prj)
+
+  # Install dependencies (required before zipping)
+  RSuite::prj_install_deps(prj)
+  expect_that_packages_installed(c("logging"), prj)
+
+  # Build project using RStudio addin
+  app <- ShinyDriver$new(app_dir)
+  app$setInputs(zip_version = "1.0")
+  app$setInputs(project_folder = params$prj_path)
+  app$setInputs(start_btn = "click", wait_ = FALSE, values_ = FALSE)
+  app$refresh()
+  app$stop()
+
+  expect_true(file.exists(file.path(app_dir, "TestProject_1.0x.zip")))
+  unlink(file.path(app_dir, "TestProject_1.0x.zip"), recursive = TRUE, force = TRUE)
+})
+
+
+test_that_shiny_app(app_dir, "Testing if rstudio_04_prj_build() error handling works", {
+  testnames <- c("non_existing_folder.R",
+                 "non_existing_prj")
+  expect_pass(testApp(appDir = app_dir, testnames = testnames,
+                      interactive = FALSE, compareImages = FALSE))
+})
